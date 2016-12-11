4838ed152523e0dddd30d7ce050987a8dffdff9c
root <napestershine@gmail.com>
2016-12-11 19:43:05 +0530

    created mutator for upper case letter -> best practices

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = ucfirst($value);
+    }
 }

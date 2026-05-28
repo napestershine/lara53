b5f9d25d5f6fc7f2c7621e745d2059db358330db
Manu <napestershine@gmail.com>
2026-05-28 07:07:22 +0200

    Remove stale bootstrap cache and add config/logging.php
    Cached config.php had hardcoded macOS paths causing CI failures.
    Added proper .gitignore to bootstrap/cache to prevent this.

--- bootstrap/cache/events.php
+++
@@ -1,9 +0,0 @@
-<?php return array (
-  'App\\Providers\\EventServiceProvider' => 
-  array (
-    'App\\Events\\SomeEvent' => 
-    array (
-      0 => 'App\\Listeners\\EventListener',
-    ),
-  ),
-);
